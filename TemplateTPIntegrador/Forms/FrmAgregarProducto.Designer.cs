2dd43f95b0c17d992851ca90982d3b7d3662adf3
tomaspore <tomasalejoalberio@gmail.com>
2024-10-24 20:11:00 -0300

    Volvi a agregar la entidad producto
    Volvi a rehacer el laburo anterior

---
+++ TemplateTPIntegrador/Forms/FrmAgregarProducto.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace TemplateTPIntegrador.Forms
+{
+    partial class FrmAgregarProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombreProducto = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lstProveedores = new System.Windows.Forms.ListBox();
+            this.btnAgregarProducto = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Categoria:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Precio:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Stock:";
+            // 
+            // txtNombreProducto
+            // 
+            this.txtNombreProducto.Location = new System.Drawing.Point(142, 53);
+            this.txtNombreProducto.Name = "txtNombreProducto";
+            this.txtNombreProducto.Size = new System.Drawing.Size(100, 26);
+            this.txtNombreProducto.TabIndex = 4;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(142, 150);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 26);
+            this.txtPrecio.TabIndex = 5;
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(142, 198);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(100, 26);
+            this.txtStock.TabIndex = 6;
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Location = new System.Drawing.Point(142, 95);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(121, 28);
+            this.cmbCategoria.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(325, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Proveedores:";
+            // 
+            // lstProveedores
+            // 
+            this.lstProveedores.FormattingEnabled = true;
+            this.lstProveedores.ItemHeight = 20;
+            this.lstProveedores.Location = new System.Drawing.Point(329, 76);
+            this.lstProveedores.Name = "lstProveedores";
+            this.lstProveedores.Size = new System.Drawing.Size(204, 164);
+            this.lstProveedores.TabIndex = 9;
+            // 
+            // btnAgregarProducto
+            // 
+            this.btnAgregarProducto.Location = new System.Drawing.Point(232, 262);
+            this.btnAgregarProducto.Name = "btnAgregarProducto";
+            this.btnAgregarProducto.Size = new System.Drawing.Size(110, 41);
+            this.btnAgregarProducto.TabIndex = 10;
+            this.btnAgregarProducto.Text = "Agregar";
+            this.btnAgregarProducto.UseVisualStyleBackColor = true;
+            this.btnAgregarProducto.Click += new System.EventHandler(this.btnAltaProducto_Click);
+            // 
+            // AgregarProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 315);
+            this.Controls.Add(this.btnAgregarProducto);
+            this.Controls.Add(this.lstProveedores);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmbCategoria);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtNombreProducto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AgregarProducto";
+            this.Text = "AgregarProducto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombreProducto;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox lstProveedores;
+        private System.Windows.Forms.Button btnAgregarProducto;
+    }
+}
--- TemplateTPIntegrador/Forms/FrmMenuAdmin.cs
+++ TemplateTPIntegrador/Forms/FrmMenuAdmin.cs
@@ -69,5 +69,14 @@ namespace TemplateTPIntegrador
             confirmacion.ShowDialog(); // Mostrar el formulario de confirmación de manera modal
 
         }
+
+        private void btnAltaProducto_Click(object sender, EventArgs e)
+        {
+            FrmAgregarProducto altaproducto = new FrmAgregarProducto();
+
+            altaproducto.Show();
+            this.Hide();
+
+        }
     }
 }
