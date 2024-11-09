ae692f5a296631ef05a7e2e0aff9dff2586c941d
brandansantiago <brandansantiago1998@gmail.com>
2024-11-08 22:17:21 -0300

    Squashed commit of the following:
    commit f02bd93ad7e98a69273b7249a360a8bbee20b285
    Author: nerinabova <nerinabova@gmail.com>
    Date:   Fri Nov 8 22:12:08 2024 -0300
    
        AgregarVentaWS

---
+++ Datos/VentasWS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class VentasWS
+    {
+        private Guid _idCliente;
+        private Guid _idUsuario;
+        private Guid _idProducto;
+        private int _cantidad;
+
+        public Guid idCliente { get { return _idCliente; } set { _idCliente = value; } }
+        public Guid idUsuario { get { return _idUsuario; } set { _idUsuario = value; } }
+        public Guid idProducto { get { return _idProducto; } set { _idProducto = value; } }
+
+        public int Cantidad { get { return _cantidad; } set { _cantidad = value; } }
+
+        public VentasWS(Guid idCliente, Guid idUsuario, Guid idProducto, int cantidad)
+        {
+            this.idCliente = _idCliente;
+            this.idUsuario = _idUsuario;
+            this.idProducto = _idProducto;
+            this.Cantidad = cantidad;
+
+
+        }
+    }
+}
