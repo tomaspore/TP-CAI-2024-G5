50e2658905762f69ba0cbe263f6d2cf20a2e95f3
nerinabova <nerinabova@gmail.com>
2024-11-14 10:19:10 -0300

    ReactivarCliente _ Tuneo de forms

--- TemplateTPIntegrador/Forms/Clientes/FrmReactivarCliente.Designer.cs
+++ TemplateTPIntegrador/Forms/Clientes/FrmReactivarCliente.Designer.cs
@@ -32,61 +32,123 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txtidCliente = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.pictureBoxIcon = new System.Windows.Forms.PictureBox();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.panelLeft.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIcon)).BeginInit();
             this.SuspendLayout();
             // 
             // btnReactivaciónCliente
             // 
-            this.btnReactivaciónCliente.Location = new System.Drawing.Point(108, 103);
-            this.btnReactivaciónCliente.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnReactivaciónCliente.BackColor = System.Drawing.Color.Purple;
+            this.btnReactivaciónCliente.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReactivaciónCliente.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnReactivaciónCliente.Location = new System.Drawing.Point(298, 177);
+            this.btnReactivaciónCliente.Margin = new System.Windows.Forms.Padding(2);
             this.btnReactivaciónCliente.Name = "btnReactivaciónCliente";
-            this.btnReactivaciónCliente.Size = new System.Drawing.Size(79, 20);
+            this.btnReactivaciónCliente.Size = new System.Drawing.Size(123, 35);
             this.btnReactivaciónCliente.TabIndex = 0;
             this.btnReactivaciónCliente.Text = "Reactivar";
-            this.btnReactivaciónCliente.UseVisualStyleBackColor = true;
+            this.btnReactivaciónCliente.UseVisualStyleBackColor = false;
             this.btnReactivaciónCliente.Click += new System.EventHandler(this.btnReactivaciónCliente_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(83, 16);
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(295, 60);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(134, 13);
+            this.label1.Size = new System.Drawing.Size(161, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "Introduzca el ID del Cliente";
             // 
             // txtidCliente
             // 
-            this.txtidCliente.Location = new System.Drawing.Point(61, 46);
-            this.txtidCliente.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtidCliente.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtidCliente.Location = new System.Drawing.Point(278, 93);
+            this.txtidCliente.Margin = new System.Windows.Forms.Padding(2);
             this.txtidCliente.Name = "txtidCliente";
-            this.txtidCliente.Size = new System.Drawing.Size(178, 20);
+            this.txtidCliente.Size = new System.Drawing.Size(178, 26);
             this.txtidCliente.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(42, 79);
+            this.label2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(246, 140);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(221, 13);
+            this.label2.Size = new System.Drawing.Size(225, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Consulte con el Administrador el ID de Cliente";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(211, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Reactivar Cliente";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackColor = System.Drawing.Color.Purple;
+            this.panelLeft.Controls.Add(this.pictureBoxIcon);
+            this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(206, 297);
+            this.panelLeft.TabIndex = 5;
+            // 
+            // pictureBoxIcon
+            // 
+            this.pictureBoxIcon.Image = global::TemplateTPIntegrador.Properties.Resources.eletrohogarx2;
+            this.pictureBoxIcon.Location = new System.Drawing.Point(30, 93);
+            this.pictureBoxIcon.Name = "pictureBoxIcon";
+            this.pictureBoxIcon.Size = new System.Drawing.Size(150, 150);
+            this.pictureBoxIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxIcon.TabIndex = 0;
+            this.pictureBoxIcon.TabStop = false;
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.BackColor = System.Drawing.Color.Gray;
+            this.buttonBack.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
+            this.buttonBack.ForeColor = System.Drawing.Color.White;
+            this.buttonBack.Location = new System.Drawing.Point(287, 252);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(150, 33);
+            this.buttonBack.TabIndex = 9;
+            this.buttonBack.Text = "Volver al Menú";
+            this.buttonBack.UseVisualStyleBackColor = false;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
             // 
             // FrmReactivarCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(293, 146);
+            this.ClientSize = new System.Drawing.Size(546, 297);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.panelLeft);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtidCliente);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnReactivaciónCliente);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FrmReactivarCliente";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Reactivar Cliente";
             this.Load += new System.EventHandler(this.FrmReactivarCliente_Load);
+            this.panelLeft.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIcon)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,5 +160,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtidCliente;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.PictureBox pictureBoxIcon;
+        private System.Windows.Forms.Button buttonBack;
     }
 }
