dc739cfdae454ff29c546767b316e220d2079229
LaiMartino96 <laimartino96@gmail.com>
2024-11-15 01:22:30 -0300

    form

--- TemplateTPIntegrador/Forms/Clientes/FrmControlClientes.cs
+++ TemplateTPIntegrador/Forms/Clientes/FrmControlClientes.cs
@@ -116,5 +116,15 @@ namespace TemplateTPIntegrador.Forms
             menu.Show();
             this.Close();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
