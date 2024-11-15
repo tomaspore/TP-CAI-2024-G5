e38026c4de9711d604bba5f31316c5422e114577
LudmilaOrellana <ludmila.orellana@yahoo.com.ar>
2024-11-15 14:11:48 -0300

    Cambios Form RC

--- TemplateTPIntegrador/Forms/Clientes/FrmReactivarCliente.Designer.cs
+++ TemplateTPIntegrador/Forms/Clientes/FrmReactivarCliente.Designer.cs
@@ -45,9 +45,10 @@
             this.btnReactivaciónCliente.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.btnReactivaciónCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnReactivaciónCliente.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnReactivaciónCliente.Location = new System.Drawing.Point(321, 274);
+            this.btnReactivaciónCliente.Location = new System.Drawing.Point(242, 190);
+            this.btnReactivaciónCliente.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnReactivaciónCliente.Name = "btnReactivaciónCliente";
-            this.btnReactivaciónCliente.Size = new System.Drawing.Size(139, 41);
+            this.btnReactivaciónCliente.Size = new System.Drawing.Size(139, 32);
             this.btnReactivaciónCliente.TabIndex = 0;
             this.btnReactivaciónCliente.Text = "Reactivar";
             this.btnReactivaciónCliente.UseVisualStyleBackColor = false;
@@ -60,9 +61,10 @@
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(316, 110);
+            this.label1.Location = new System.Drawing.Point(213, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 25);
+            this.label1.Size = new System.Drawing.Size(92, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Ingrese  ID ";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -71,9 +73,10 @@
             // txtidCliente
             // 
             this.txtidCliente.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtidCliente.Location = new System.Drawing.Point(321, 144);
+            this.txtidCliente.Location = new System.Drawing.Point(214, 93);
+            this.txtidCliente.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtidCliente.Name = "txtidCliente";
-            this.txtidCliente.Size = new System.Drawing.Size(241, 35);
+            this.txtidCliente.Size = new System.Drawing.Size(222, 26);
             this.txtidCliente.TabIndex = 2;
             // 
             // label2
@@ -81,9 +84,10 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Goldenrod;
             this.label2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(317, 205);
+            this.label2.Location = new System.Drawing.Point(211, 139);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(352, 21);
+            this.label2.Size = new System.Drawing.Size(225, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Consulte con el Administrador el ID de Cliente";
             // 
@@ -93,9 +97,10 @@
             this.label3.BackColor = System.Drawing.Color.Indigo;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Goldenrod;
-            this.label3.Location = new System.Drawing.Point(316, 33);
+            this.label3.Location = new System.Drawing.Point(211, 21);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(274, 37);
+            this.label3.Size = new System.Drawing.Size(195, 26);
             this.label3.TabIndex = 4;
             this.label3.Text = "Reactivar Cliente";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -106,18 +111,16 @@
             this.panelLeft.Controls.Add(this.pictureBoxIcon);
             this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelLeft.Location = new System.Drawing.Point(0, 0);
-            this.panelLeft.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panelLeft.Name = "panelLeft";
-            this.panelLeft.Size = new System.Drawing.Size(294, 457);
+            this.panelLeft.Size = new System.Drawing.Size(196, 297);
             this.panelLeft.TabIndex = 5;
             // 
             // pictureBoxIcon
             // 
             this.pictureBoxIcon.Image = global::TemplateTPIntegrador.Properties.Resources.eletrohogarx2;
-            this.pictureBoxIcon.Location = new System.Drawing.Point(26, 110);
-            this.pictureBoxIcon.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBoxIcon.Location = new System.Drawing.Point(17, 72);
             this.pictureBoxIcon.Name = "pictureBoxIcon";
-            this.pictureBoxIcon.Size = new System.Drawing.Size(225, 231);
+            this.pictureBoxIcon.Size = new System.Drawing.Size(150, 150);
             this.pictureBoxIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxIcon.TabIndex = 0;
             this.pictureBoxIcon.TabStop = false;
@@ -127,10 +130,9 @@
             this.buttonBack.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.buttonBack.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
             this.buttonBack.ForeColor = System.Drawing.Color.White;
-            this.buttonBack.Location = new System.Drawing.Point(614, 401);
-            this.buttonBack.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonBack.Location = new System.Drawing.Point(242, 234);
             this.buttonBack.Name = "buttonBack";
-            this.buttonBack.Size = new System.Drawing.Size(179, 42);
+            this.buttonBack.Size = new System.Drawing.Size(139, 31);
             this.buttonBack.TabIndex = 9;
             this.buttonBack.Text = "Volver al Menú";
             this.buttonBack.UseVisualStyleBackColor = false;
@@ -138,10 +140,10 @@
             // 
             // FrmReactivarCliente
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Indigo;
-            this.ClientSize = new System.Drawing.Size(819, 457);
+            this.ClientSize = new System.Drawing.Size(448, 297);
             this.Controls.Add(this.buttonBack);
             this.Controls.Add(this.panelLeft);
             this.Controls.Add(this.label3);
@@ -149,6 +151,7 @@
             this.Controls.Add(this.txtidCliente);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnReactivaciónCliente);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FrmReactivarCliente";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Reactivar Cliente";
